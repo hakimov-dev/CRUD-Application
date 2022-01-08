438dff8df484f4b15b80faf9992478a4d0aa4392
Muhammadamin <example@gmail.com>
2022-01-08 12:28:13 +0500

    Created userList.vue

---
+++ src/components/userList.vue
@@ -0,0 +1,17 @@
+<template>
+  
+</template>
+
+<script>
+export default {
+  name:"userList",
+  data() {
+      return {
+          
+      }
+  },
+  methods:{},
+  props:{},
+  emits:{}
+}
+</script>
