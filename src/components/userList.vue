a7e2a1c823be3080787232affcec515a777cf7e5
Muhammadamin <example@gmail.com>
2022-01-08 18:02:49 +0500

    The keywords we need were extracted from the array.

--- src/components/userList.vue
+++ src/components/userList.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="card" v-for="person in user" :key="person">
-      <h3>{{person}}</h3>
+  <div class="card" v-for="person in user" :key="person.id">
+      <h3>{{person.firstName}}</h3>
   </div>
     <div class="card center">
     <h4>There are no user yet</h4>
