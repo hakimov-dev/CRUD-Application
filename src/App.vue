cb75b9a5daf7bbfa021a55b39ad370e643dd2751
Muhammadamin <example@gmail.com>
2022-01-08 12:16:47 +0500

    Created new variables

--- src/App.vue
+++ src/App.vue
@@ -24,14 +24,17 @@ export default {
     }
   },
   methods:{
-    createUser(){
-     fetch( 'https://crudd-app-hakimov-default-rtdb.firebaseio.com/users.json', {
+    async createUser(){
+    
+    const response = await fetch( 'https://crudd-app-hakimov-default-rtdb.firebaseio.com/users.json', {
        method:'POST',
        headers:{'Content-Type': 'application/json'},
        body: JSON.stringify({
          firstName: this.name
        })
      })
+
+     const firebaseData = await response.json()
     }
   }
 }
