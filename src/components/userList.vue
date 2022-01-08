e88cb7f80caf3e2a5db4239b1b4d318506ded5d5
Muhammadamin <example@gmail.com>
2022-01-08 15:08:22 +0500

    Error in 'v-for' fixed.

--- src/components/userList.vue
+++ src/components/userList.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="card" :v-for="peson in user" :key="person">
+  <div class="card" :v-for="person in user" :key="person">
       <h3>{{person}}</h3>
   </div>
     <div class="card center">
