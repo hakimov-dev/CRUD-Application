daa6347a527bb2f4533168eb95266e7d820db97d
Muhammadamin <example@gmail.com>
2022-01-08 12:39:57 +0500

    Create class 'card' for load users

--- src/components/userList.vue
+++ src/components/userList.vue
@@ -2,6 +2,10 @@
   <div class="card">
       <h3></h3>
   </div>
+    <div class="card center">
+    <h4>There are no user yet</h4>
+    <button class="btn primary">Load people</button>
+  </div>
 </template>
 
 <script>
