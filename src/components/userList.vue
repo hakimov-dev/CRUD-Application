43f5d829dd64be91757d07536d6638b156f8cb96
Hakimov-dev <webhacker0100@gmail.com>
2022-01-08 19:39:25 +0500

    Add a parrent elements

--- src/components/userList.vue
+++ src/components/userList.vue
@@ -1,10 +1,12 @@
 <template>
-  <div class="card" v-for="person in user" :key="person.id">
+  <div>
+      <div class="card" v-for="person in user" :key="person.id">
       <h3>{{person.firstName}}</h3>
   </div>
     <div class="card center">
     <h4>There are no user yet</h4>
     <button @click="$emit('loadUser')" class="btn primary">Load people</button>
+  </div>
   </div>
 </template>
 
