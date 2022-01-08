e4d1ee4cc265ee538e2f407ba49f19b792e7e6f5
Muhammadamin <example@gmail.com>
2022-01-08 13:28:30 +0500

    An entry was written from inside the 'user' Array.

--- src/components/userList.vue
+++ src/components/userList.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="card" :v-for="peson in user" :key="person">
-      <h3></h3>
+      <h3>{{person}}</h3>
   </div>
     <div class="card center">
     <h4>There are no user yet</h4>
