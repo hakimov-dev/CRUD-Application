c3a3bf425840f1e8bdeca419bd64a486198bd770
Hakimov-dev <webhacker0100@gmail.com>
2022-01-09 13:58:10 +0500

    Add card class

--- src/components/TheModal.vue
+++ src/components/TheModal.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="alert" v-if="alert" :class="alert.type">
+  <div class="card alert" v-if="alert" :class="alert.type">
    <h2>{{alert.title}}</h2>
    <p>{{alert.text}}</p>
    <button class="btn" :class="alert.type" @click="$emit('closeModal')">Close</button>
