23982de413ecb79ef039caaf6a82fee75bc6f4b4
Hakimov-dev <webhacker0100@gmail.com>
2022-01-09 11:39:16 +0500

    Add click event and send '(closeModal)' in App.vue

--- src/components/TheModal.vue
+++ src/components/TheModal.vue
@@ -2,7 +2,7 @@
   <div class="alert" v-if="alert" :class="alert.type">
    <h2>{{alert.title}}</h2>
    <p>{{alert.text}}</p>
-   <button class="btn" :class="alert.type">Close</button>
+   <button class="btn" :class="alert.type" @click="$emit('closeModal')">Close</button>
   </div>
 </template>
 
