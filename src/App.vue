39ba008efe0139db529ea8ec6286d5900d1d0bef
Hakimov-dev <webhacker0100@gmail.com>
2022-01-09 09:31:47 +0500

    Remove diynamic atribute.

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,7 @@
 
    <button :disabled="name.length === 0" class="btn">Create user</button>
   </form>
-  <user-list :user="user" @loadUser="loadUser" @remove="remove"/>
+  <user-list :user="user" @loadUser="loadUser" @remove="remove(id)"/>
 </div>
 </template>
 
