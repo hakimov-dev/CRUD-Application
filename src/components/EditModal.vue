47b2de7b7aac382f1af2c39afa001a20c7495e8a
Hakimov-dev <webhacker0100@gmail.com>
2022-01-09 21:08:16 +0500

    Created 'EditModal.vue' and Added template tag

---
+++ src/components/EditModal.vue
@@ -0,0 +1,10 @@
+<template>
+    <form class="modal card" @submit.prevent="">
+   <div class="form-control">
+     <label for="name">Edit your name</label>
+     <input type="text" id="name" placeholder="Edit name" v-model.trim="name">
+   </div>
+
+   <button class="btn">Edit</button>
+  </form>
+</template>
