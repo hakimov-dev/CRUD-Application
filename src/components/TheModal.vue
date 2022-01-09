335f5f4dc2d5cd6fb12eb19c88f0615ef13be43a
Hakimov-dev <webhacker0100@gmail.com>
2022-01-09 11:37:38 +0500

    The button added dynamic class

--- src/components/TheModal.vue
+++ src/components/TheModal.vue
@@ -2,7 +2,7 @@
   <div class="alert" v-if="alert" :class="alert.type">
    <h2>{{alert.title}}</h2>
    <p>{{alert.text}}</p>
-   <button class="btn">Close</button>
+   <button class="btn" :class="alert.type">Close</button>
   </div>
 </template>
 
